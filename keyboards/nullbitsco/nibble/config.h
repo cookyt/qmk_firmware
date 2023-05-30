12a5ef46787d669c9a2225bab36bb68b66577b9d
Jay Greco <jayv.greco@gmail.com>
2023-05-30 06:03:46 +0000

    RP2040 build

--- keyboards/nullbitsco/nibble/config.h
+++ keyboards/nullbitsco/nibble/config.h
@@ -27,13 +27,3 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 16
 #define MATRIX_MUX_COLS 4
-
-/*
- * Keyboard Matrix Assignments
- * The nibble uses a demultiplexer for the cols.
- * to free up more IOs for awesomeness!
- * See matrix.c for more details.
-*/
-#define MATRIX_ROW_PINS { B1, B3, B2, B6, D4 }
-#define MATRIX_COL_MUX_PINS { F4, F5, F6, F7 }
-#define MATRIX_COL_PINS { }
